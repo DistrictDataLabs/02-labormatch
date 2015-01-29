51e2e1ecfed6fee934ae41a81a44a9125fb948dd
Keegan Hines <keegan.hines@gmail.com>
2015-01-28 21:47:39 -0500

    new class for BLS api

--- labormatch/BSL_ingest.py
+++ labormatch/BSL_ingest.py
@@ -1,16 +1,35 @@
+import requests
 
 
-def main():
-  import requests
-  seriesID='LAUCN040010000000005'
-  p=requests.get('http://api.bls.gov/publicAPI/v2/timeseries/data/' + seriesID)
-  series = p.json()['Results']['series'][0]['data']
-
-
-  with open('../fixtures/exampleData.json', 'w') as writefile:
-      for item in series:
+class BLSrequest():
+    def __init__(self):
+        self.api_base='http://api.bls.gov/publicAPI/v2/timeseries/data/' 
+        
+    def get(self,input_str):
+        p=requests.get(self.api_base + input_str)
+        self.response=p.json()['Results']['series'][0]['data']
+        
+        
+    def post(self, input_str):    
+        p=requests.post(self.api_base + input_str)
+        return p # TO DO
+    
+    def write(self,fileName):
+      with open(fileName, 'w') as writefile:
+        for item in self.response:
           writefile.write(str(item) +'\n')  
     
+
+def main():
+
+  exampleSeries='LAUCN040010000000005'
+  api=BLSrequest()
+  api.get(exampleSeries)
+  api.write('../fixtures/exampleData.json')
+  
+
+
+
     
 if __name__ == "__main__":
   main()
