999ff03068e835f5528516e042ce7d87a1047b28
Yanan <yanansun7@gmail.com>
2015-02-03 21:23:54 -0500

    using OAuth 1.0a
    using OAuth 1.0a instead of using OAuth 2.0 for authentication

--- labormatch/authentication.py
+++ labormatch/authentication.py
@@ -2,15 +2,33 @@
 #yanan
 #not working yet
 
-from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication,
-                               PERMISSIONS)
+from linkedin import linkedin # pip install python-linkedin
+
+# OAuth 1.0a
+# Define CONSUMER_KEY, CONSUMER_SECRET,  
+# USER_TOKEN, and USER_SECRET from the credentials 
+# provided in your LinkedIn application
 
 
-if __name__ == '__main__':
-    API_KEY = '77t43u3loazvrz'
-    API_SECRET = 'b95o3WkJj7vbNWMj'
-    RETURN_URL = 'http://localhost:8000'
-    authentication = LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL,
-                                            PERMISSIONS.enums.values())
-    print authentication.authorization_url
-    application = LinkedInApplication(authentication)
+CONSUMER_KEY = '77t43u3loazvrz'
+CONSUMER_SECRET = 'b95o3WkJj7vbNWMj'
+
+USER_TOKEN = 'd77d38f5-6f3e-422a-9a6a-6fe985ae8c54'
+USER_SECRET = '01027166-7044-4c73-988a-61896e12dd82'
+
+RETURN_URL = 'http://ysun1.github.io' # Not required for developer authentication
+
+# Instantiate the developer authentication class
+
+auth = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET, 
+                                USER_TOKEN, USER_SECRET, 
+                                RETURN_URL, 
+                                permissions=linkedin.PERMISSIONS.enums.values())
+
+# Pass it in to the app...
+
+app = linkedin.LinkedInApplication(auth)
+
+# Use the app...
+
+app.get_profile()
