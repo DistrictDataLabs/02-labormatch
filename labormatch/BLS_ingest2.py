af984253911ac8a57b932322dab7c73f6f0e79a9
Yanan <yanansun7@gmail.com>
2015-03-11 21:17:20 -0400

    Update BLS_ingest2.py

--- labormatch/BLS_ingest2.py
+++ labormatch/BLS_ingest2.py
@@ -1,7 +1,6 @@
 import requests
 import json
-import prettytable
-import sys
+import csv
 
 
 
@@ -20,50 +19,42 @@ def getcode(fname):
     
 
 def main():
-    
-    if len(sys.argv)>1:
-        key = str(sys.argv[1])
-    else:
-        key='8c3ab1a673a340d39b89f1823419ee79'
-    print key
-    
-    prefix = 'BD'
-    seasonal = 'S'
-    unit = '0'
-    sizeclass = '01'
-    recordtype = 'Q'
-    ownership = '5'
+        
+    prefix = 'SM'
+    seasonal = 'U'
+    area='00000'
+    industry = '60540000'
+    datatype = '01'
 
-    ratelevel = 'L'
-    dataelement = '1'
-    industry_codes = ['100020', '100030','200080']
-    dataclass = '03'
+    #f_area = 'bd.state.txt'
+    #area_codes = getcode(f_area)
+    f_state = 'sm.state.txt'
+    states = getcode(f_state)
 
-    f_area = 'bd.state.txt'
-    area_codes = getcode(f_area)
+    fout = open('sm_series_list.txt', 'w')
+    fno = open('sm_series_list_noresutls.txt','w')
     
-    fout = open('bd_series_list.txt', 'w')
     series_list = []
-    for areacode in area_codes:
-        for industry in industry_codes:
-            seriesid = prefix + seasonal + areacode + industry + unit + dataelement+ sizeclass + dataclass +ratelevel + recordtype + ownership
-            series_list.append(seriesid)
-            fout.write(seriesid)
-            fout.write('\n')
-    #series_groups = [series_list[x:x+50] for x in xrange(0, len(series_list), 50)] #only take 50 per multi-series request                                      
-            headers = {'Content-type': 'application/json'}
-    #for group in series_groups:                                                                                                                                
+    for state in states:
+        seriesid = prefix + seasonal + state + area + industry + datatype
+        series_list.append(seriesid)
+        fout.write(seriesid)
+        fout.write('\n')
+        series_groups = [series_list[x:x+50] for x in xrange(0, len(series_list), 50)] #only take 50 per multi-series request
+        headers = {'Content-type': 'application/json'}
+        for group in series_groups:
             data = json.dumps({
                     "seriesid": [seriesid],
                     "startyear": "2011",
                     "endyear": "2014"
                     })
-            
+            key='8c3ab1a673a340d39b89f1823419ee79'
             p = requests.post('http://api.bls.gov/publicAPI/v2/timeseries/data/', data = data, headers = headers, auth=(key,''))
             json_data = json.loads(p.text)
             if json_data['status'].find('REQUEST_NOT_PROCESSED')<0:
                 for series in json_data['Results']['series']:
                     for item in series['data']:
+                        
                         row = {
                             "series_id": series["seriesID"],
                             "year": item['year'],
@@ -71,18 +62,23 @@ def main():
                             "value": item['value'],
                             "has_footnotes": False
                             }
+                        
                         if len(item["footnotes"]) > 2:
                             row["has_footnotes"] = True
-                            output = open('output/BLS_'+ seriesid + '.txt','w')
-                            output.write (row)
-
+                            
+                        with open('output/sm_data.csv', 'a') as outfile:
+                            writer = csv.DictWriter(outfile, ["series_id", "year", "period", "value", "has_footnotes"])
+                            writer.writerow(row)
+                        outfile.close()
+                            
+ 
             else:
+                print seriesid
+                fno.write(seriesid)
+                fno.write('\n')
+                print json_data['status']
                 print json_data['message']
-                   
-
-    
-
-       
+                
     
 if __name__ == "__main__":
   main()
